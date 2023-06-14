65b00b8ca135dc438083c0753b4c9c52feb125c1
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-13 18:03:29 -0700

    feat: ✨ update search component

--- components/main/SearchComponent.vue
+++ components/main/SearchComponent.vue
@@ -5,10 +5,44 @@
     >
       <Icon name="tabler:world-search" size="30" />
       <input
+        v-model="inputTerm"
         type="text"
         class="mx-2 w-[450px] border-transparent bg-transparent px-2 text-2xl focus:outline-none focus:ring-0"
         placeholder="Search"
+        @keyup.enter="runSearch"
       />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const inputTerm = ref("");
+
+const searchEngines = ["google", "bing", "yahoo", "duckduckgo"];
+
+const searchEngine = ref(searchEngines[0]);
+
+const runSearch = () => {
+  const searchTerm = inputTerm.value;
+
+  if (searchTerm !== "") {
+    switch (searchEngine.value) {
+      case "google":
+        window.open(`https://www.google.com/search?q=${searchTerm}`);
+        break;
+      case "bing":
+        window.open(`https://www.bing.com/search?q=${searchTerm}`);
+        break;
+      case "yahoo":
+        window.open(`https://search.yahoo.com/search?p=${searchTerm}`);
+        break;
+      case "duckduckgo":
+        window.open(`https://duckduckgo.com/?q=${searchTerm}`);
+        break;
+      default:
+        window.open(`https://www.google.com/search?q=${searchTerm}`);
+        break;
+    }
+  }
+};
+</script>
